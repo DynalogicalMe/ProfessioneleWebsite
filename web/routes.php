aff3a6909511a83a2b00092c41ef41c1de8dee65
DESKTOP-H628LQ8\stefa <stefan.klok@student.stenden.com>
2017-11-07 11:58:02 +0100

    mijn pagina's
    aangemeld, geslaagd en studiekeuzepagina

--- web/routes.php
+++ web/routes.php
@@ -8,3 +8,9 @@ Router::add('', 'home', 'home');
 Router::add('studenten', 'studenten', 'studenten');
 
 Router::add('contact', 'contact', 'contact');
+
+Router::add('geslaagd', 'geslaagd', 'geslaagd');
+
+Router::add('aangemeld', 'aangemeld', 'aangemeld');
+
+Router::add('studiekeuze', 'studiekeuze', 'studiekeuze');
