37ab0f8b22afbf7164bb493de793a87b13efe20a
DynalogicalMe <stef.van.zwienen@student.stenden.com>
2017-11-08 21:20:02 +0100

    Added Nicks branch

--- resources/view/docenten.php
+++ resources/view/docenten.php
@@ -1,74 +1,403 @@
-<div class="teacherBox">
-	<div class="teacherPicBox">
-		<img class="teacherPic" src="public/assets/teacher_1.jpg" alt="Docent">
-	</div>
-	<div class="teacherTextBox">
-		<p class="teacherText">Niko Menten</p>
-	</div>
-	<div class="teacherStatusBox StatusBoxGreen">
-		<p class="teacherStatusBoxText">Beschikbaar</p>
-	</div>
-	<div class="teacherSubjectBox">
-		<p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
-	</div>
+<div id="teacherHeaderBox">
+    <h2 id="teacherHeaderBoxText">Docenten</h2>
 </div>
 
-<div class="teacherBox">
-	<div class="teacherPicBox">
-		<img class="teacherPic" src="public/assets/teacher_2.jpg" alt="Docent">
-	</div>
-	<div class="teacherTextBox">
-		<p class="teacherText">Levina Radder</p>
-	</div>
-	<div class="teacherStatusBox StatusBoxRed">
-		<p class="teacherStatusBoxText">Ziek</p>
-	</div>
-	<div class="teacherSubjectBox">
-		<p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
-	</div>
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Raymond_Blankestijn.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Raymond Blankestijn</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> Minorcoördinator</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;raymond.blankestijn@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123146</p>
+        </div>
+    </div>
 </div>
 
-<div class="teacherBox">
-	<div class="teacherPicBox">
-		<img class="teacherPic" src="public/assets/teacher_3.jpg" alt="Docent">
-	</div>
-	<div class="teacherTextBox">
-		<p class="teacherText">Fredrik Melchers</p>
-	</div>
-	<div class="teacherStatusBox StatusBoxGreen">
-		<p class="teacherStatusBoxText">Beschikbaar</p>
-	</div>
-	<div class="teacherSubjectBox">
-		<p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
-	</div>
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Ischa_Berghuis.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Ischa Berghuis</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> Projectbegeleider</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;ischa.Berghuis@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123145</p>
+        </div>
+    </div>
 </div>
 
-<div class="teacherBox">
-	<div class="teacherPicBox">
-		<img class="teacherPic" src="public/assets/teacher_4.jpg" alt="Docent">
-	</div>
-	<div class="teacherTextBox">
-		<p class="teacherText">Melissa Hogenbirk</p>
-	</div>
-	<div class="teacherStatusBox StatusBoxGreen">
-		<p class="teacherStatusBoxText">Beschikbaar</p>
-	</div>
-	<div class="teacherSubjectBox">
-		<p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
-	</div>
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Bert_Meijerink.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Bert Meijerink</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> Stage-afstudeercoördinator</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;bert.meijerink@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123114</p>
+        </div>
+    </div>
 </div>
 
-<div class="teacherBox">
-	<div class="teacherPicBox">
-		<img class="teacherPic" src="public/assets/teacher_5.jpg" alt="Docent">
-	</div>
-	<div class="teacherTextBox">
-		<p class="teacherText">Annemiek Mullenders</p>
-	</div>
-	<div class="teacherStatusBox StatusBoxGreen">
-		<p class="teacherStatusBoxText">Beschikbaar</p>
-	</div>
-	<div class="teacherSubjectBox">
-		<p class="teacherText"><i class="material-icons">accessibility</i> SLB</p>
-	</div>
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Jeroen_Pijpker.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Jeroen Pijpker</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> Teamleider</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;jeroen.pijpker@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123117</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Thijs_Smegen.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Thijs Smegen</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> Onderwijsassistent</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;thijs.smegen@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123119</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Elleke_Jagersma.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Elleke Jagersma</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">accessibility</i> SLB</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;elleke.jagersma@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123110</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Johan_ten_Brink.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Johan ten Brink</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;johan.ten.brink@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123148</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Niels_Doorn.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Niels Doorn</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;niels.doorn@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123149</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Albert_de_Jonge.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Albert de Jonge</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;albert.de.jonge@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123111</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Rob_Loves.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Rob Loves</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;rob.loves@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123113</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Victor_Peters.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Victor Peters</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">music_note</i> Muziek</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;victor.peters@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123116</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Rene_van_Bolhuis.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">René van Bolhuis</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;rene.van.bolhuis@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123147</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Rene_Laan.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">René Laan</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;rene.laan@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123112</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Jochen_den_Ouden.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Jochen den Ouden</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxRed">
+            <p class="teacherStatusBoxText">Ziek</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;jochen.den.ouden@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123115</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Winnie_van_Schilt.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Winnie van Schilt</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;winnie.van.schilt@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123118</p>
+        </div>
+    </div>
+</div>
+
+<div class="teacherBoxContainer">
+    <div class="teacherBox">
+        <div class="teacherPicBox">
+            <img class="teacherPic" src="public/assets/Rob_Smit.jpg" alt="Docent">
+        </div>
+        <div class="teacherTextBox">
+            <p class="teacherText">Rob Smit</p>
+        </div>
+        <div class="teacherStatusBox StatusBoxGreen">
+            <p class="teacherStatusBoxText">Beschikbaar</p>
+        </div>
+        <div class="teacherSubjectBox">
+            <p class="teacherText"><i class="material-icons">attach_money</i> Economie</p>
+        </div>
+    </div>
+    <div class="teacherExtraInfoBox">
+        <div class="teacherExtraLeftBox">
+            <p class="teacherExtraLeftText"><i class="material-icons">email</i>&nbsp;rob.smit@mondriaan.com</p>
+        </div>
+        <div class="teacherExtraRightBox">
+            <p class="teacherExtraRightText"><i class="material-icons">phone_android</i>&nbsp;0619123120</p>
+        </div>
+    </div>
 </div>
